ba8deb2c9a3fa9ab337d19a6eb5d46edebf82bac
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2013-05-13 22:11:12 -0400

    lang

--- usinacv/usinacv/urls.py
+++ usinacv/usinacv/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf.urls.i18n import i18n_patterns
+
 from django.conf import settings
 
-urlpatterns = patterns('',
+urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
