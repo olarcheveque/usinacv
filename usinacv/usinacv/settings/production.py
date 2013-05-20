9ba5a86766252a46aa2ca240298422cf337b6ffb
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2013-05-20 15:04:45 -0400

    remove from env

--- usinacv/usinacv/settings/production.py
+++ usinacv/usinacv/settings/production.py
@@ -5,19 +5,6 @@ from os import environ
 
 from base import *
 
-# Normally you should not import ANYTHING from Django directly
-# into your settings, but ImproperlyConfigured is an exception.
-from django.core.exceptions import ImproperlyConfigured
-
-
-def get_env_setting(setting):
-    """ Get the environment setting or return exception """
-    try:
-        return environ[setting]
-    except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)
-
 INSTALLED_APPS += ('gunicorn',)
 
 ########## EMAIL CONFIGURATION
@@ -69,9 +56,3 @@ CACHES = {
     }
 }
 ########## END CACHE CONFIGURATION
-
-
-########## SECRET CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = get_env_setting('SECRET_KEY')
-########## END SECRET CONFIGURATION
